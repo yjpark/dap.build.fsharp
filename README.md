c0d814725516d79bfbcb4ddaa8b2e26eb51b0499
YJ Park <yjpark@gmail.com>
2018-07-23 16:59:46 +0800

    split Dap.Build to individual repo

---
+++ README.md
@@ -0,0 +1,6 @@
+## Dependencies
+
+### TaskBuilder
+
+- https://github.com/rspeele/TaskBuilder.fs
+- https://github.com/fsharp/fslang-suggestions/issues/581
---
+++ build.fsx
@@ -0,0 +1,19 @@
+#r "paket: groupref Build //"
+#load ".fake/build.fsx/intellisense.fsx"
+
+open Fake.Core
+open Fake.IO.Globbing.Operators
+
+#load "src/Dap.Build/DotNet.fs"
+#load "src/Dap.Build/NuGet.fs"
+module NuGet = Dap.Build.NuGet
+
+let feed : NuGet.Feed = {
+    NuGet.Source = "https://nuget.yjpark.org/nuget/dap"
+    NuGet.ApiKey = NuGet.Plain "wnHZEG9N_OrmO3XKoAGT"
+}
+
+let projects =
+    !! "src/Dap.Build/*.fsproj"
+
+NuGet.createAndRun NuGet.release feed projects
